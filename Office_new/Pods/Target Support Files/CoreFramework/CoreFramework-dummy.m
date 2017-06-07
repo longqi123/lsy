11fe93b4d0ba23a82f70550780655eb577d20e7f
梁仕友 <lsy@lsy.local>
2017-06-07 22:14:17 +0800

    ddd

--- Office_new/Pods/Target Support Files/CoreFramework/CoreFramework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CoreFramework : NSObject
-@end
-@implementation PodsDummy_CoreFramework
-@end
--- Office_new/Pods/Target Support Files/Pods-Office/Pods-Office-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Office : NSObject
-@end
-@implementation PodsDummy_Pods_Office
-@end
